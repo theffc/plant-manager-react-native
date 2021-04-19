d5ec867a5257e06ffe85b51f5d5ce33978569be0
theffc <frederico.calhau@zup.com.br>
2021-04-19 20:23:29 -0300

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>asdfasdf</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
